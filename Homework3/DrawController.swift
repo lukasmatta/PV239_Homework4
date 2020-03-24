858d694c8ee2610d8bbb43461b19c6f3dd4faa2b
Lukas Matta <lukas@matta.sk>
2020-03-24 16:42:06 +0100

    Finish next homework

--- Homework3/DrawController.swift
+++ Homework3/DrawController.swift
@@ -11,7 +11,12 @@ import UIKit
 class DrawController: UIViewController {
     weak var itemsListDelegate: ItemsListDelegate?
 
+    @IBOutlet weak var buttons: UIStackView!
+    @IBOutlet weak var loadingSpinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
+        loadingSpinner.isHidden = true
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -34,9 +39,63 @@ class DrawController: UIViewController {
     }
     
     private func drawNumberAndClose(from: Int, to: Int, color: UIColor) {
-        let newItem = ColoredNumber(value: Int.random(in: from...to), color: color)
-        itemsListDelegate?.add(item: newItem)
-        presentingViewController?.dismiss(animated: true, completion: nil)
+        var randomNumber: Int = 0;
+        
+        let url = URL(string: "https://www.random.org/integers/?num=1&min=\(from)&max=\(to)&base=10&format=plain&col=1")
+        guard let requestUrl = url else { fatalError() }
+
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if error != nil {
+                DispatchQueue.main.async {
+                    self.stopLoading()
+                    let errorMessage = error.debugDescription
+                    self.showError(errorMessage: errorMessage)
+                }
+            }
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                let trimmedString = dataString.trim()
+                print(trimmedString)
+                if let generatedNumber = Int(trimmedString) {
+                    randomNumber = generatedNumber
+                }
+                DispatchQueue.main.async {
+                    let newItem = ColoredNumber(value: randomNumber, color: color)
+                    self.itemsListDelegate?.add(item: newItem)
+                    self.presentingViewController?.dismiss(animated: true, completion:                     self.stopLoading)
+                }
+            }
+        }
+
+        task.resume()
+        startLoading()
+    }
+    
+    private func startLoading() {
+        buttons.isHidden = true
+        loadingSpinner.isHidden = false
+        loadingSpinner.startAnimating()
+    }
+    
+    private func stopLoading() {
+        loadingSpinner.stopAnimating()
+        loadingSpinner.isHidden = true
+        buttons.isHidden = false
+    }
+    
+    private func showError(errorMessage: String) {
+        let alert = UIAlertController(title: "An error occured :(", message: errorMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension String {
+    func trim() -> String {
+          return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
 }
 
